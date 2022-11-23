4c7c74f6ce2bad70b67070ee59168680fe89b7d1
Gaspar Balam <l19070043@valladolid.tecnm.mx>
2022-11-23 15:58:29 -0600

    Se realizo un cambio de codigo en el archivo css y se agrego un vistas.php

---
+++ vistas.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Leer - CRUD PHP</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        table td {
+            border: 1px solid orange;
+            text-align: center;
+            padding: 1.3rem;
+        }
+        .button {
+            border-radius: .5rem;
+            color: white;
+            background-color: orange;
+            padding: 1rem;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <p><a class="button" href="nuevo.php">Crear</a></p>
+    <table>
+        <tr>
+            <th>Código</th>
+            <th>Título</th>
+            <th>Autor</th>
+            <th>¿Disponible?</th>
+            <td></td>
+            <td></td>
+        </tr>
+    
+    </table>
+</body>
+</html>
